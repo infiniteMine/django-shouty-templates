infiniteMine/django-shouty-templates

commit 4b1b3bdc8a702b2b6b23b51cb0799df2291c027f

Change to using py2 compatible type annotation syntax for appconfig's ready method
Fixes #1

shouty.py
@@ -175,7 +175,8 @@ class Shout(AppConfig):
     """
     name = "shouty"
 
-    def ready(self) -> bool:
+    def ready(self)
+        # type: () -> bool
         logger.info("Applying shouty templates patch")
         return patch(
             invalid_variables=getattr(settings, "SHOUTY_VARIABLES", True),

commit 72b4ae6dad48de740dfd3234eb78a4f5811d3d54

Fix syntax error introduced in 4b1b3bd while "fixing" #1

shouty.py
@@ -187,7 +187,7 @@ class Shout(AppConfig):
     """
     name = "shouty"
 
-    def ready(self)
+    def ready(self):
         # type: () -> bool
         logger.info("Applying shouty templates patch")
         return patch(